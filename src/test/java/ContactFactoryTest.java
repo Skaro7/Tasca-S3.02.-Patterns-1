088c80cf214a9b404f5de5e8d6298b6c98f0806d
Skaro7 <marc_tau72@hotmail.com>
2026-02-24 19:53:20 +0100

    test: add tests for ContactFactory implementations

--- src/test/java/ContactFactoryTest.java
+++ src/test/java/ContactFactoryTest.java
@@ -1,6 +1,4 @@
-import level2.Address;
-import level2.ContactFactory;
-import level2.SpainAddress;
+import level2.*;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -8,8 +6,35 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class ContactFactoryTest {
 
     @Test
+    void testSpainContact() {
+        ContactFactory factory = new SpainContactFactory(
+                "Carrer Major 34", "Barcelona", "08001", "123456789"
+        );
+        Contact contact = new Contact(factory);
+        assertEquals("Address: Carrer Major 34, 08001 Barcelona | Phone: +34 123456789",
+                contact.toString());
+    }
+
+    @Test
     void testSpainAddressFormat() {
-        Address addr = new SpainAddress("Carrer Major 34", "Barcelona", "08001");
-        assertEquals("Carrer Major 34, 08001 Barcelona", addr.format());
+        Address address = new SpainAddress("Carrer Major 34", "Barcelona", "08001");
+        assertEquals("Carrer Major 34, 08001 Barcelona", address.format());
+    }
+
+    @Test
+    void testUSAContact() {
+        ContactFactory factory = new USAContactFactory(
+                " 154 5th Avenue", "New York", "NY 10001", "987654321"
+        );
+        Contact contact = new Contact(factory);
+        assertEquals("Address: 154 5th Avenue, New York, NY 10001 | Phone: +1 987654321",
+                contact.toString());
+    }
+
+    @Test
+    void testUSAPhoneFormat() {
+        Phone phone = new USAPhone("212555000");
+        assertEquals("+1 212555000", phone.format());
     }
 }
+
